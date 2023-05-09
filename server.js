dcd122be916e3c6494352e9abe06a01037fc034e
Menno Vlaming <mennovlaming@gmail.com>
2023-05-09 16:22:26 +0200

    fetch

--- server.js
+++ server.js
@@ -30,17 +30,17 @@ io.on('connection', socket => {
   //   .catch(error => console.error(error))
 
 
-  socket.on('chat message', message => {
-    // If the message is "!nextquote", fetch a new quote and emit it to all clients
-    if (message === '!nextquote') {
-      fetch('https://api.kanye.rest/')
-        .then(response => response.json())
-        .then(data => {
-          io.emit('kanye-quote', data.quote);
-        })
-        .catch(error => console.error(error));
-    }
-  });
+  // socket.on('chat message', message => {
+  //   // If the message is "!nextquote", fetch a new quote and emit it to all clients
+  //   if (message === '!nextquote') {
+  //     fetch('https://api.kanye.rest/')
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         io.emit('kanye-quote', data.quote);
+  //       })
+  //       .catch(error => console.error(error));
+  //   }
+  // });
 
   
   socket.on('disconnect', () => {
